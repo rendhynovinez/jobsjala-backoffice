9818b960399c858bbe7cad6911c330967ecaf230
FANNYLEED\Mona <0215027rendynovian@gmail.com>
2021-12-17 20:58:07 +0700

    fix customer list show data group

--- app/Http/Controllers/CustomerController.php
+++ app/Http/Controllers/CustomerController.php
@@ -16,7 +16,7 @@ class CustomerController extends Controller
 
     public function index()
     {
-        $Customer = Customer::all();
+        $Customer = Customer::whereNull('is_group')->get();
 
         return view('admin.listcustomers', ['customers' => $Customer]);
     }

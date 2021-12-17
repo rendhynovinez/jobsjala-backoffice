623135a51994c89af80b50b70dfea73368a71b42
FANNYLEED\Mona <0215027rendynovian@gmail.com>
2021-12-17 20:18:21 +0700

    (+) Update default Group production

--- app/Http/Controllers/ApiController.php
+++ app/Http/Controllers/ApiController.php
@@ -197,7 +197,7 @@ class ApiController extends Controller
             ]);
 
             if($request->Group === null){
-                $group = 27;
+                $group = 1;
             }else{
                 $group = $request->Group;
             }
